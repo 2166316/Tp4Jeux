8640c31b9f1ef2d7394873201b152b3bd6886133
Mathcegep <144719358+Mathcegep@users.noreply.github.com>
2024-05-28 11:22:38 -0400

    ausio setup 2

--- Tp4/Assets/scripts/Player/audioPlayer.cs
+++ Tp4/Assets/scripts/Player/audioPlayer.cs
@@ -7,27 +7,41 @@ public class audioPlayer : MonoBehaviour
     public AudioSource ASource;
     public AudioClip AOutdoors;
     public AudioClip AInside;
+    private Rigidbody rb;
+
+    private string outside = "outside";
+    private string inside = "inside";
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rb.velocity.magnitude > 0)
+        {
+            typeGround();
+        }
     }
 
-    bool typeGround()
+    void typeGround()
     {
         RaycastHit hit;
-        bool isgrounded = false;
         if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.1f))
         {
-            isgrounded = true;
+            if (hit.collider.CompareTag("Exterior"))
+            {
+                playFootStepsAudio(AOutdoors);
+            }
         }
-        return isgrounded;
+    }
+
+    void playFootStepsAudio(AudioClip audio)
+    {
+        ASource.pitch = 0.5f;
+        //ASource.PlayOneShot(audio);
     }
 }
