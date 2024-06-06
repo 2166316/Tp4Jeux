edb899e3c4110281669817ceb8a6b5aa6dd61d96
Mathcegep <144719358+Mathcegep@users.noreply.github.com>
2024-06-06 14:22:08 -0400

    finished moving obstacle outside

--- Tp4/Assets/scripts/Navmesh/MoveBench.cs
+++ Tp4/Assets/scripts/Navmesh/MoveBench.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class MoveBench : MonoBehaviour
 {
     public Transform targetObject;
-    public float rotationSpeed = 5f;
+    private float rotationSpeed = 40f;
 
     private void Update()
     {
-        // Get the position of the target object
         Vector3 targetPosition = targetObject.position;
 
-        // Rotate around the target object's position on the y-axis
         transform.RotateAround(targetPosition, Vector3.up, rotationSpeed * Time.deltaTime);
     }
 }
